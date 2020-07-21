7241bcf44cc7c471ba95a79d0a580a66ff2ac4fd
GILBERTO JUNIOR DOS SANTOS <giljrsantos@gmail.com>
2020-07-21 08:49:57 -0300

    Preview Link React Js

---
+++ README.md
@@ -0,0 +1,36 @@
+# PROJETO VISUALIZADOR DE LINKS COM REACTJS
+
+Projeto em React Js, para recuperar os dados das `tag <meta>`.
+
+O FaceBook, Twitter, Whatsapp usam um metado parecido, parar recuperar os dados quando um link é postado em um novo post ou comentário.
+
+
+
+Este projeto foi iniciado com [Create React App](https://github.com/facebook/create-react-app).
+
+## 1º Passo:
+`npx create-react-app <nomedoprojeto>`
+
+## 2º Passo:
+`cd <nomedoprojeto>`
+## 3º Passo:
+## `Efetuar o clone do projeto`
+
+## 3º Passo
+### `npm start`
+
+# DEPENDÊNCIAS
+    "axios": "^0.19.2",
+    "bootstrap": "^4.5.0",
+    "cheerio": "^1.0.0-rc.3",
+[Axios](https://github.com/axios/axios),
+
+[BootStrap](https://www.npmjs.com/package/bootstrap),
+
+[Cheerio](https://github.com/cheeriojs/cheerio)
+
+
+## SCREENSHOT
+<p align="center">
+<img src="./src/img/Screenshot_1.png" title="Tela principal">
+</p>
---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import './App.css';
+
+const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+
+const getPreviewData = (tags ) => {
+  const result = tags.reduce((previewData, item) => {
+    switch(item.tag){
+      case 'og:title':
+        previewData.title = item.value;
+        break;
+      case 'og:url':
+        previewData.link = item.value;
+        break;
+      case 'og:description':
+        previewData.description = item.value;
+        break;
+      case 'og:site_name':
+        previewData.site = item.value;
+        break;
+      case 'og:image':
+        previewData.image = item.value;
+        break;
+      default:
+      
+    }
+    return previewData
+  }, {});
+  return Promise.resolve(result);
+};
+
+const parseHTML = (html) => {
+
+  const $ = cheerio.load(html);
+
+  const meta = [];
+
+  $('head meta').map((i, item) => {
+    if (!item.attribs) return null;
+
+    const property = item.attribs.property || null;
+    const content = item.attribs.content || null;
+    
+    if (!property || !content) return null;
+    
+    meta.push({tag: property, value: content});
+
+    return null;
+
+  });
+
+  return Promise.resolve(meta);
+
+};
+
+const fetchUrl = (url) => {
+  return axios(CORS_PROXY + url)
+    .then(response => response.data);
+};
+
+const getUrl = (text) => {
+  if (!text) return null;
+
+  const a = document.createElement('a');
+  a.href = text;
+
+  const { protocol, host, pathname, search, hash} = a;
+
+  const url = `${protocol}//${host}${pathname}${search}${hash}`;
+
+  const isSameHost = (host === window.location.host);
+  
+  if(isSameHost) return null;
+  
+  return url;
+};
+
+function App() {
+
+  const [ previewData, setPreviewData ] = useState(null);
+
+  const onBlur = (e) =>{
+    const url = getUrl(e.target.value);
+    if(!url) return null;
+    fetchUrl(url)
+      .then(parseHTML)
+      .then(getPreviewData)
+      .then(setPreviewData)
+      .then(console.log)
+      .catch(console.error);
+  };
+
+  console.log('previewData', previewData);
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6">
+          <div className="form-group">
+            <label>Url</label>
+            <input type="text" className="form-control" onBlur={onBlur} />
+          </div>
+        </div>
+      </div>
+      {previewData && (
+        <div className="row">
+          <div className="card col-6 pt-3">
+            <img className="card-img-top" src={previewData.image} width="250" alt={previewData.title} />
+            <div className="card-body">
+              <small>{previewData.site}</small>
+              <h5 className="card-title">{previewData.title}</h5>
+              <p>{previewData.description}</p>
+              <a href={previewData.link} target="_blank" rel="noopener noreferrer" className="btn btn-primary">Leia Mais...</a>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
